9a0bd87d3dbbfdef3343fc4af1abedb005af46d7
johney <johneyburke@gmail.com>
2023-09-09 21:40:37 -0600

    flair

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(msg = "go to the office"){
+
+    return `This Monday, I will ${msg}.`
+}
+
+function wrapAdjective(flair = "*"){
+    const inner = function(msg = "special"){
+    return `You are ${flair}${msg}${flair}!`
+    }
+        return inner;
+    }
+
+
+
